0933f710c9adbc30a92102c86ca4b2015cad4b4c
Tom Rochette <roctom@gmail.com>
2013-04-24 10:10:24 -0400

    Fixed missing path() => asset() conversions.

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -35,9 +35,9 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="{{ asset('js/vendor/jquery-1.8.2.min.js') }}"><\/script>')</script>
-        <script src="{{ path('js/vendor/prettify.js') }}"></script>
-        <script src="{{ path('js/plugins.js') }}"></script>
-        <script src="{{ path('js/main.js') }}"></script>
+        <script src="{{ asset('js/vendor/prettify.js') }}"></script>
+        <script src="{{ asset('js/plugins.js') }}"></script>
+        <script src="{{ asset('js/main.js') }}"></script>
 
         <script>
             var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
